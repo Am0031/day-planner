5511d198e12f4279aed38c6ecb30016d5aebff74
Amelie Pira <amelie.pira@gmail.com>
2022-06-21 17:23:56 +0100

    added draft sql files

---
+++ src/data/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (14, "Yoga class");
+INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (10, "Walk the dog");
+INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (12, "Lunch with AJ");
+INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (16, "Meeting with Yanis");
