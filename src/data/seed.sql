52ae479202b3105f8ae77d0f162f73a5279724a6
Amelie Pira <amelie.pira@gmail.com>
2022-06-21 17:41:15 +0100

    edited seed.sql

--- src/data/seed.sql
+++ src/data/seed.sql
@@ -1,4 +1,1 @@
-INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (14, "Yoga class");
-INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (10, "Walk the dog");
-INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (12, "Lunch with AJ");
-INSERT INTO `day_panner_db`.`tasks` (`hour`, `taskText`) VALUES (16, "Meeting with Yanis");
+INSERT INTO tasks(hour, taskText) VALUES (10, "Walk the dog"),(14, "Yoga class"),(12, "Lunch with AJ"),(16, "Meeting with Yanis");
